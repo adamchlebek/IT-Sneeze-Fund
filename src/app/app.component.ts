80d8de358cb84c5a00fb487fc781f8fcd4c3cffe
Adam Chlebek <adam.chlebek@dominionsystems.local>
2019-06-28 13:47:38 -0400

    Updated Banner Display and Updated Old Cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ import { User } from './user';
 export class AppComponent {
   title = 'IT Sneeze Fund';
   public clickedEvent: User;
-  text: boolean = false;
+  text: boolean = true;
 
   constructor(private userAPI: UserAPIService){
   }
